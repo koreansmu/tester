aebc57cd7430777b186dad668f08b5fe15f53778
koreansmu <shuklakusum4q@gmail.com>
2025-11-19 15:18:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "27696582"))
 API_HASH = os.getenv("API_HASH", "45fccefb72a57ff1b858339774b6d005")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_USERNAME = os.getenv("BOT_USERNAME", "BillaGuardianBot")
 
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
 
@@ -15,8 +16,8 @@ SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "").split(","))) if os.getenv
 
 LOGGER_ID = int(os.getenv("LOGGER_ID", "-1002398675912"))  # Group/Channel ID for logging
 
-BOT_USERNAME = os.getenv("BOT_USERNAME", "GuardXBot")
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/your_support_chat")
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/BillaSpace")
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "https://t.me/BillaCore")
 
 DEFAULT_LANG = "en"
 CACHE_TTL = 3600  # 1 hour caching for now
